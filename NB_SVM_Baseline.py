f81b1b7ebbab9a606658d7f02bf7dd4b518e9792
Euphoria Yan <ysq58000@foxmail.com>
2019-06-07 20:07:43 +0800

    Create NB_SVM.

--- NB_SVM_Baseline.py
+++ NB_SVM_Baseline.py
@@ -1,4 +1,60 @@
-import pandas as pd, numpy as np
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+from keras.models import Model
+from keras.layers import Input, Dense, Embedding, concatenate
+from keras.layers import Bidirectional, GRU, GlobalMaxPool1D, GlobalAveragePooling1D, SpatialDropout1D, Dropout
+from keras.preprocessing import text, sequence
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+import warnings
+import os
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.linear_model import LogisticRegression
+from scipy import sparse
+
+
+class NbSvmClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, C=1.0, dual=False, n_jobs=1):
+        self.C = C
+        self.dual = dual
+        self.n_jobs = n_jobs
+
+    def predict(self, x):
+        # Verify that model has been fit
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict(x.multiply(self._r))
+
+    def predict_proba(self, x):
+        # Verify that model has been fit
+        check_is_fitted(self, ['_r', '_clf'])
+        return self._clf.predict_proba(x.multiply(self._r))
+
+    def fit(self, x, y):
+        # Check that X and y have correct shape
+        y = y.values
+        x, y = check_X_y(x, y, accept_sparse=True)
+
+        def pr(x, y_i, y):
+            p = x[y == y_i].sum(0)
+            return (p+1) / ((y == y_i).sum()+1)
+
+        self._r = sparse.csr_matrix(np.log(pr(x, 1, y) / pr(x, 0, y)))
+        x_nb = x.multiply(self._r)
+        self._clf = LogisticRegression(C=self.C, dual=self.dual, n_jobs=self.n_jobs).fit(x_nb, y)
+        return self
+
+
+if __name__ == '__main__':
+
+    train = pd.read_csv('./input/jigsaw-toxic-comment-classification-challenge/train_preprocessed.csv')
+    test = pd.read_csv('./input/jigsaw-toxic-comment-classification-challenge/test_preprocessed.csv')
+    submission = pd.read_csv('./input/jigsaw-toxic-comment-classification-challenge/sample_submission.csv')
+    test_label = pd.read_csv('./input/jigsaw-toxic-comment-classification-challenge/test_labels.csv')
 
