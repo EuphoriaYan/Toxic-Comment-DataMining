3a1b714d26f21fb06a318842c3b8e74f25fe6708
Euphoria Yan <ysq58000@foxmail.com>
2019-06-16 22:40:33 +0800

    NB_SVM didn't fit on negative numbers.

--- NB_SVM_Baseline.py
+++ NB_SVM_Baseline.py
@@ -3,22 +3,10 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
-from keras.models import Model
-from keras.layers import Input, Dense, Embedding, concatenate
-from keras.layers import Bidirectional, GRU, GlobalMaxPool1D, GlobalAveragePooling1D, SpatialDropout1D, Dropout
-from keras.preprocessing import text, sequence
-from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
-import warnings
-import os
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.linear_model import LogisticRegression
-from scipy import sparse
-import get_embedding
 
 
 class NbSvmClassifier(BaseEstimator, ClassifierMixin):
@@ -39,15 +27,16 @@ class NbSvmClassifier(BaseEstimator, ClassifierMixin):
 
     def fit(self, x, y):
         # Check that X and y have correct shape
-        y = y.values
-        x, y = check_X_y(x, y, accept_sparse=True)
+        # y = y.values
+        # x, y = check_X_y(x, y, accept_sparse=True)
 
         def pr(x, y_i, y):
             p = x[y == y_i].sum(0)
             return (p+1) / ((y == y_i).sum()+1)
 
-        self._r = sparse.csr_matrix(np.log(pr(x, 1, y) / pr(x, 0, y)))
-        x_nb = x.multiply(self._r)
+        self._r = np.log(pr(x, 1, y) / pr(x, 0, y))
+        # x_nb = x.multiply(self._r)
+        x_nb = np.multiply(x, self._r)
         self._clf = LogisticRegression(C=self.C, dual=self.dual, n_jobs=self.n_jobs).fit(x_nb, y)
         return self
 
@@ -68,7 +57,9 @@ if __name__ == '__main__':
 
     X_tra, X_val, y_tra, y_val = train_test_split(X_features, y_train, train_size=0.95, random_state=233)
 
-    model = NbSvmClassifier(C=4, dual=True, n_jobs=-1).fit(X_tra, y_tra)
+    NbSvmClassifier_list = list()
+    for i in range(y_train.shape[1]):
+        model = NbSvmClassifier(C=4, dual=True, n_jobs=-1).fit(X_tra, y_tra[:, i])
 
     y_pred = model.predict(y_val)
     score = roc_auc_score(y_val, y_pred)
